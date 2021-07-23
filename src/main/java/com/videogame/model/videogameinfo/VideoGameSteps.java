fe81cd7802a637e855e9a1773569e26217d8ff9d
mk4london <81772843+mk4london@users.noreply.github.com>
2021-07-23 00:17:17 +0000

    my homework week 19

---
+++ src/main/java/com/videogame/model/videogameinfo/VideoGameSteps.java
@@ -0,0 +1,4 @@
+package com.videogame.model.videogameinfo;
+
+public class VideoGameSteps {
+}
---
+++ src/test/java/com/videogame/model/testbase/TestBase.java
@@ -0,0 +1,20 @@
+package com.videogame.model.testbase;
+
+import com.videogame.model.constants.Path;
+import com.videogame.model.utils.PropertyReader;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class TestBase
+{
+    public static PropertyReader propertyReader;
+
+    @BeforeClass
+    public static void init(){
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+        RestAssured.port = Integer.parseInt(propertyReader.getProperty("port"));
+        RestAssured.basePath = Path.VIDEOGAME;
+    }
+
+}
